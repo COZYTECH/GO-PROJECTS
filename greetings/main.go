5156a28c2d3d7e42765d59ccc1c40f19ce13f95d
COZYTECH <109769074+COZYTECH@users.noreply.github.com>
2026-02-09 13:25:49 +0100

    imporoved

--- greetings/main.go
+++ greetings/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"math/rand"
 )
 
@@ -11,16 +13,38 @@ func main() {
 	fmt.Print("Enter your name: ")
 	//reads user input and stores it in the variable name
 	fmt.Scanln(&name)
+
+    // Pick a random greeting
+    // msg := greetings[rand.Intn(len(greetings))]
+
+    // // Print it
+    // fmt.Printf(msg+"\n", name)
+    // //fmt.Println("Hello, " + name + "!")
+
+	message, err := Hello(name)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println(message)
+}
+func Hello(name string) (string, error) {
+    if name == "" {
+        return "", errors.New("empty name provided")
+    }
+
+    greeting := randomGreeting()
+    return fmt.Sprintf(greeting, name), nil
+}
+
+func randomGreeting() string {
     greetings := []string{
         "Hi, %v! Welcome!",
         "Great to see you, %v!",
         "Hail, %v! Well met!",
+        "Howdy, %v!",
+        "Hello there, %v!",
     }
 
-    // Pick a random greeting
-    msg := greetings[rand.Intn(len(greetings))]
-
-    // Print it
-    fmt.Printf(msg+"\n", name)
-    //fmt.Println("Hello, " + name + "!")
+    return greetings[rand.Intn(len(greetings))]
 }
